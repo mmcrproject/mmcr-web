a131920c00359b63262eeef624cb68a642bdee26
Gary Harper <salesgl@live.com>
2013-10-25 17:27:03 -0400

    Redoing whole tree due to crash

---
+++ responsive/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML PUBLIC 
+  "-//W3C//DTD HTML 4.01 Transitional//EN"
+  "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+<HTML>
+<HEAD>
+<TITLE>MMCR</TITLE>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+<link rel="stylesheet" href="./css/fonts.css" type="text/css">
+<link rel="stylesheet" href="./css/masterStyles.css" type="text/css">
+<link rel="stylesheet" href="./css/index.css" type="text/css">
+</HEAD>
+
+<BODY>
+
+<div id="nav-header">
+	<img id="logo" src="./img/logo.png"/>
+	<img id="logo-mobile" src="./img/logo-mobile.png"/>
+	<div id="navbar">
+		
+		<div id="navbarLinks">
+			<span class="black orangebg">Home</span>
+			<span class="black redbg">Adopt</span>
+			<span class="black bluebg">Donate</span>
+			<span class="black purplebg">Volunteer</span>
+			<span class="black greenbg">Learn</span>
+		</div>
+	</div>
+	<div id="navbarMobile">
+		<span class="arrow-down"></span><span class="brown">Menu</span>
+	</div>
+	<div class="clear"> </div>
+</div> <!--/header -->
+<div id="headerImage">
+	<span class="header">Live Love Learn</span>
+</div>
+<span id="siteDescription" class="bold">Mid-Michigan Cat Rescue is dedicated to promoting the health and happiness of our cats and the humans who adopt them.</span>
+
+
+<div class="clear spacer"></div>
+
+<div id="contentSelectorDiv">
+	<table id="contentSelectorTable">
+		<tr>
+			<td id="contentSelector1">
+				<span>Adopt</span>
+			</td>
+			<td class="space"></td>
+			<td id="contentSelector2">
+				<span>Donate</span>
+			</td>
+			<td class="space"></td>
+			<td id="contentSelector3">
+				<span>Volunteer</span>
+			</td>
+			<td class="space"></td>
+			<td id="contentSelector4">
+				<span>How You Can Help</span>
+			</td>
+			<td class="space"></td>
+			<td id="contentSelector5">
+				<span>What We Do</span>
+			</td>
+			<td class="space"></td>
+			<td id="contentSelector6">
+				<span>Who We Are</span>
+			</td>
+		</tr>
+	</table>
+</div>
+
+<div class="clear spacer"></div>
+
+<div id="content">
+
+	<div class="contentItem">
+		<div class="contentHeadline">
+			<span>Headline One</span>
+		
+<div class="clear spacer"></div>
+
+		</div>
+		<div class="contentStory">
+			<img src="./img/wordpress.png" class="contentLeftImage"></img>
+			<span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</span>
+		</div>
+	</div>
+
+<div class="clear spacer"></div>
+	
+	<div class="contentItem">
+		<div class="contentHeadline">
+			<span>Headline Two Special Stuff</span>
+			
+<div class="clear spacer"></div>
+			
+		</div>
+		<div class="contentStory">
+			<img src="./img/twitter.png" class="contentRightImage"></img>
+			<span>Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.</span>
+		</div>
+	</div>
+
+<div class="clear spacer"></div>
+	
+	<div class="contentItem">
+		<div class="contentHeadline">
+			<span>This is Headline Three</span>
+			
+<div class="clear spacer"></div>
+
+		</div>
+		<div class="contentStory">
+			<img src="./img/twitter.png" class="contentLeftImage"></img>
+			<img src="./img/wordpress.png" class="contentRightImage"></img>
+			<span>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</span>
+		</div>
+	</div>
+	
+</div>
+
+</BODY>
+</HTML>
